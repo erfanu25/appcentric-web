acb539da04484e7d1b0be75091da5d60888a107e
Erfan <mderfan2@gmail.com>
2020-07-12 23:03:23 +0600

    Add Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppHeaderComponent} from './layouts/full/header/header.component';
+import {AppSidebarComponent} from './layouts/full/sidebar/sidebar.component';
+import {FullComponent} from './layouts/full/full.component';
+import {SharedModule} from './shared/shared.module';
+import {SpinnerComponent} from './shared/spinner.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutes} from './app.routing';
+import {DemoMaterialModule} from './demo-material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppSidebarComponent,
+    FullComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    DemoMaterialModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
